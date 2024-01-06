b7afb238fb5b06ebc5438d198ac73027582591ec
jloveric <john.loverich@gmail.com>
2024-01-06 07:34:03 -0800

    Improve loss function

--- deep_organize/loss.py
+++ deep_organize/loss.py
@@ -3,7 +3,7 @@ from typing import Callable
 
 def equidistance2d_loss(x : torch.Tensor, target:float=1.0) :
     dist=torch.cdist(x,x,p=2)
-    return torch.sum(dist-target)
+    return torch.sum(torch.pow(dist-target,2))
 
 def within_region_loss(x: torch.Tensor, distance_function : Callable[[torch.Tensor], float]):
     pass
